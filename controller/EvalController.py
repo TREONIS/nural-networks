bfb23b99c46d09e3fa10d26946feeb6eec5aa963
Bohdanio <141421634+TREONIS@users.noreply.github.com>
2023-08-12 01:35:11 +0300

    Update EvalController.py

--- controller/EvalController.py
+++ controller/EvalController.py
@@ -1,7 +1,6 @@
 class EvaluadorController:
     def __init__(self) -> None:
-        super().__init__()
-        # FIXME convertirlo en diccionario cuya llave sea el id del estudiante y el valor una lista de las evaluaciones que se han hecho para el mismo estudiante
+        super().__init__()       
         self.evaluaciones = []
 
     def agregar_evaluacion(self, evaluacion_obj):
